e4d8d4ad28f15af669915b9eec3b87f7afd7bb5f
Navot Oz <t-navotoz@microsoft.com>
2023-07-04 10:32:20 +0300

    handle borders in optim single frame

--- utils/optim_correspondance.py
+++ utils/optim_correspondance.py
@@ -28,6 +28,7 @@ def find_diff(*, warped, mask, static):
     d = mask_to_nans(image=warped, mask=mask)
     s = mask_to_nans(image=static, mask=mask)
     diff = np.abs(s-d)
+    diff /= 100  # 100C to C
     return diff[~np.isnan(diff)].mean(), diff
 
 
@@ -51,48 +52,6 @@ def warp(*, pts: Union[str, Path], dynamic, static):
 def mp_warp(pts, dynamic, static):
     warped, mask = warp(pts=pts, dynamic=dynamic, static=static)
     return find_diff(warped=warped, mask=mask, static=static)[0]
-
-
-def optim_single_pts(*, path: Union[str, Path],
-                     n_pixels_for_single_points: int = 0,
-                     idx_of_frame: int):
-    dynamic = np.load(path / f"left_{idx_of_frame}.npy")
-    static = np.load(path / f"right_{idx_of_frame}.npy")
-    pts, colors = load_pts(path_to_points=path / f'points_{idx_of_frame}.csv')
-    loss_init = mp_warp(dynamic=dynamic, static=static, pts=pts)
-    loss_best = loss_init
-
-    # Optimize all points as single points first
-    warper = partial(mp_warp, dynamic=dynamic, static=static)
-    permutations = np.arange(- n_pixels_for_single_points, n_pixels_for_single_points+1)
-    with tqdm(total=np.prod(pts.shape), desc='Optimize single points') as pbar:
-        for i, j in np.ndindex(pts.shape):
-            pts_ = pts.copy().values[None, ...].repeat(len(permutations), 0)
-            pts_[..., i, j] += permutations
-            pts_list = list(pts_)
-            pts_list = [pd.DataFrame(p, columns=pts.columns, index=pts.index) for p in pts_list]
-            with Pool(cpu_count()) as p:
-                losses = list(p.imap(warper, pts_list))
-            # losses = list(tqdm(map(warper, pts_list), total=len(pts_list), desc=f'Optimize single points {i},{j}'))
-            loss = min(losses)
-            if loss < loss_best:
-                pts_best = pts_list[np.argmin(losses)]
-                pts_best.index = colors
-                if not all(pts_best >= 0):
-                    print('Negative values in points!')
-                    print(pts_best)
-                    break
-                pts_best.astype(int).to_csv(path / f'points_{idx_of_frame}.csv', index=True)
-                pts = pts_best
-                loss_best = loss
-            pbar.set_postfix_str(f'Loss: Init {loss_init:.2f}, Best {loss_best:.2f}')
-            pbar.update()
-
-
-def generate_permutations(N, SIZE):
-    elements = np.arange(N + 1)  # Generate elements from 0 to N
-    permutations = list(product(elements, repeat=SIZE))
-    return np.array(permutations)
 
 
 def optim_best_pts(*, path: Union[str, Path], idx_of_frame: int, n_pixels_to_permute: int = 0, n_runs: int = 100):
@@ -130,8 +89,60 @@ def optim_best_pts(*, path: Union[str, Path], idx_of_frame: int, n_pixels_to_per
         pts = pts_best
         loss_init = loss
 
+
+def optim_single_pts(*, path: Union[str, Path],
+                     n_pixels_for_single_points: int = 0,
+                     idx_of_frame: int):
+    dynamic = np.load(path / f"left_{idx_of_frame}.npy")
+    static = np.load(path / f"right_{idx_of_frame}.npy")
+    pts, colors = load_pts(path_to_points=path / f'points_{idx_of_frame}.csv')
+    loss_init = mp_warp(dynamic=dynamic, static=static, pts=pts)
+    loss_best = loss_init
+
+    # Optimize all points as single points first
+    warper = partial(mp_warp, dynamic=dynamic, static=static)
+    permutations = np.arange(- n_pixels_for_single_points, n_pixels_for_single_points+1)
+    with tqdm(total=np.prod(pts.shape), desc='Optimize single points') as pbar:
+        for i, j in np.ndindex(pts.shape):
+            value_of_point = pts.iloc[i, j]
+            dim_of_point = pts.columns[j]
+            if 'x' in dim_of_point.lower():
+                dim_of_point = 1
+            elif 'y' in dim_of_point.lower():
+                dim_of_point = 0
+            permutations_bounded_by_image = permutations[(value_of_point + permutations >= 0) &
+                                                         (value_of_point + permutations < dynamic.shape[dim_of_point])]
+            pts_ = pts.copy().values[None, ...].repeat(len(permutations_bounded_by_image), 0)
+            pts_[..., i, j] += permutations_bounded_by_image
+            pts_list = list(pts_)
+            pts_list = [pd.DataFrame(p, columns=pts.columns, index=pts.index) for p in pts_list]
+            with Pool(cpu_count()) as p:
+                losses = list(p.imap(warper, pts_list))
+            # losses = list(tqdm(map(warper, pts_list), total=len(pts_list), desc=f'Optimize single points {i},{j}'))
+            loss = min(losses)
+            if loss < loss_best:
+                pts_best = pts_list[np.argmin(losses)]
+                pts_best.index = colors
+                if not all(pts_best >= 0):
+                    print('Negative values in points!')
+                    print(pts_best)
+                    break
+                pts_best.astype(int).to_csv(path / f'points_{idx_of_frame}.csv', index=True)
+                pts = pts_best
+                loss_best = loss
+            pbar.set_postfix_str(f'Loss: Init {loss_init:.2f}, Best {loss_best:.2f}')
+            pbar.update()
+
+
+def generate_permutations(N, SIZE):
+    elements = np.arange(N + 1)  # Generate elements from 0 to N
+    permutations = list(product(elements, repeat=SIZE))
+    return np.array(permutations)
+
+
 IDX_OF_FRAME = 25
-optim_single_pts(path=Path('rawData'), n_pixels_for_single_points=50, idx_of_frame=IDX_OF_FRAME)
+for _ in range(5):
+    optim_single_pts(path=Path('rawData'), n_pixels_for_single_points=200, idx_of_frame=IDX_OF_FRAME)
 # optim_best_pts(path=Path('rawData'), n_pixels_to_permute=1, idx_of_frame=IDX_OF_FRAME)
 
 # import matplotlib.pyplot as plt
@@ -161,4 +172,3 @@ optim_single_pts(path=Path('rawData'), n_pixels_for_single_points=50, idx_of_fra
 # plt.title('Difference, Avg error: {:.2f}C'.format(diff[mask].mean()))
 # plt.colorbar()
 # plt.show()
-
