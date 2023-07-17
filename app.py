00af8ee876253b8627b4e19fe5a2bc147832ffc4
Navot Oz <t-navotoz@microsoft.com>
2023-07-17 11:29:27 +0300

    deprecated thread_devices into app.py

--- app.py
+++ app.py
@@ -2,10 +2,23 @@ from pathlib import Path
 import shutil
 from uuid import uuid4
 from flask import Flask, render_template
-import threading as th
-from thread_devices import ThreadDevices, NAME_DEVICES_THREAD
+from functools import partial
+from time import sleep
+import multiprocessing as mp
+
+from devices import INIT_CAMERA_PARAMETERS, EnumParameterPosition
+from devices.CameraProcess import CameraCtrl
 
 app = Flask(__name__)
+
+
+def _make_status(self, stat: int):
+    stat = EnumParameterPosition(stat)
+    if stat == EnumParameterPosition.DONE:
+        return 'Ready'
+    else:
+        return stat.name
+
 
 # Create a random save folder, to avoid error in RPi timestamp
 path_to_save = Path().cwd() / 'measurements' / str(uuid4().hex)
@@ -16,9 +29,24 @@ while True:
     else:
         path_to_save = Path().cwd() / 'measurements' / str(uuid4().hex)
 
-if NAME_DEVICES_THREAD not in map(lambda x: x.name, th.enumerate()):
-    thread_devices = ThreadDevices(path_to_save=path_to_save)
-    thread_devices.start()
+# Init two cameras - Panchromatic and Monochromatic
+params = INIT_CAMERA_PARAMETERS
+params['ffc_mode'] = 'auto'
+params['ffc_period'] = 3600  # ffc every one minute
+
+# Init cameras
+# Cams are synced by the barrier, which releases all cams simultaneously when N_CAMERAS acquire it.
+N_CAMERAS = 2
+barrier_camera_sync = mp.Barrier(parties=N_CAMERAS)
+func_cam = partial(CameraCtrl, camera_parameters=params, is_dummy=False,
+                   barrier_camera_sync=barrier_camera_sync,
+                   time_to_save=5e9)  # dump to disk every 5 seconds
+camera_pan = func_cam(path_to_save=path_to_save / 'pan', name='pan')
+camera_pan.start()
+camera_mono = func_cam(path_to_save=path_to_save / 'mono', name='mono')
+while camera_pan.camera_parameters_setting_position != EnumParameterPosition.DONE.value:
+    sleep(1)
+camera_mono.start()
 
 
 @app.route('/delete_all')
@@ -36,12 +64,12 @@ def delete():
 def index():
     return render_template(
         'camera_stats.html',
-        pan_status=thread_devices.status_pan,
-        pan_rate=thread_devices.rate_pan,
-        pan_files=thread_devices.n_files_pan,
-        mono_status=thread_devices.status_mono,
-        mono_rate=thread_devices.rate_mono,
-        mono_files=thread_devices.n_files_mono)
+        pan_status=_make_status(camera_pan._param_setting_pos.value),
+        pan_rate=camera_pan.rate_camera,
+        pan_files=camera_pan.n_files_saved,
+        mono_status=_make_status(camera_mono._param_setting_pos.value),
+        mono_rate=camera_mono.rate_camera,
+        mono_files=camera_mono.n_files_saved)
 
 
 if __name__ == '__main__':
